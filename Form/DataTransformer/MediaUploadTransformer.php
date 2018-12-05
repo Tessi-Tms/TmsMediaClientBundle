0cf6ac00218668dcbec9d381bb25aaffaa8f1a96
Mencé <remy.mence@tessi.fr>
2018-12-05 18:07:35 +0100

    remove useless method

--- Form/DataTransformer/MediaUploadTransformer.php
+++ Form/DataTransformer/MediaUploadTransformer.php
@@ -12,22 +12,6 @@ use Symfony\Component\Form\DataTransformerInterface;
 class MediaUploadTransformer implements DataTransformerInterface
 {
     /**
-     * Transforms.
-     *
-     * @param object|null $media
-     *
-     * @return object|null
-     */
-    public function transform($media)
-    {
-        if (null !== $media && null !== $media->getUploadedFile()) {
-            return $media;
-        }
-
-        return null;
-    }
-
-    /**
      * Reverse transforms.
      *
      * @param object|null $media
